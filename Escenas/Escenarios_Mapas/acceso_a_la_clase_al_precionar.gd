2ae73d307aea96e6cf1a96db59ab64358ebc1ac9
David <dquispellocallasi@gmai>
2025-07-14 12:35:53 -0500

    "audios clase 3"

--- Escenas/Escenarios_Mapas/acceso_a_la_clase_al_precionar.gd
+++ Escenas/Escenarios_Mapas/acceso_a_la_clase_al_precionar.gd
@@ -14,6 +14,8 @@ func cambio_escena() ->void:
 			get_tree().change_scene_to_file("res://Plan_de_Estudio/Primer_Semestre/clase_00/clase_0.tscn")
 		1:
 			get_tree().change_scene_to_file("res://Plan_de_Estudio/Primer_Semestre/clase_01/clase_1.tscn")
+		3: 
+			get_tree().change_scene_to_file("res://Plan_de_Estudio/Primer_Semestre/clase_03/clase_3_1.tscn")
 		_:
 			GLOBAL.numero_clase = 0
 			get_tree().change_scene_to_file("res://Plan_de_Estudio/Primer_Semestre/clase_00/clase_0.tscn")
