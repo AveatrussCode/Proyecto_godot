9b7a9a8ed629d9e60ba71692fd5ae8555e6efb87
Luzied <168470140+Luzied@users.noreply.github.com>
2025-07-14 22:23:39 -0500

    asd

--- Plan_de_Estudio/Primer_Semestre/clase_01/clase_1.gd
+++ Plan_de_Estudio/Primer_Semestre/clase_01/clase_1.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 func _ready() -> void:
 	Dialogic.timeline_ended.connect(_on_dialogic_timeline_end)
-	Dialogic.start("res://Escenas/Plan_de_Estudio/Primer_Semestre/clase_02/clase_1_1.dtl")
+	Dialogic.start("res://Plan_de_Estudio/Primer_Semestre/clase_01/clase_1_1.dtl")
 
 func _on_dialogic_timeline_end() -> void:
 	GLOBAL.create_transition(self, 2)
