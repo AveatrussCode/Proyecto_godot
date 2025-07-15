6ee18d8af24892240ff054f0bea8e1a2b206ff10
Luzied <168470140+Luzied@users.noreply.github.com>
2025-07-15 11:18:32 -0500

    asd

---
+++ Plan_de_Estudio/Primer_Semestre/clase_05/clase_5_1.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready() -> void:
+	Dialogic.timeline_ended.connect(_on_dialogic_timeline_end)
+	Dialogic.start("res://Plan_de_Estudio/Primer_Semestre/clase_05/clase_05_1.dtl")
+
+func _on_dialogic_timeline_end() -> void:
+	GLOBAL.create_transition(self, 2)
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_file("res://Escenas/Escenarios_Mapas/clase_computacion.tscn")
